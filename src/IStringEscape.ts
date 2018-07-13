231fc8ea7b5f0b77e212e023e364334443659349
sdaiweiy <daiwei018>
2018-07-13 15:37:36 +0800

    补充escapeHtml和unEscapeHtml的方法实现

--- src/IStringEscape.ts
+++ src/IStringEscape.ts
@@ -16,7 +16,17 @@ export default class IStringEscape {
      * @return {String} html编码后的字符串
      */
     static escapeHtml(source: string): string {
-        return '';
+        let s = "";
+        if (source.length == 0)
+            return "";
+
+        s = source.replace(/&/g, "&amp;");
+        s = s.replace(/</g, "&lt;");
+        s = s.replace(/>/g, "&gt;");
+        s = s.replace(/ /g, "&nbsp;");
+        s = s.replace(/\'/g, "&#39;");
+        s = s.replace(/\"/g, "&quot;");
+        return s;
     }
 
     /**
@@ -25,7 +35,17 @@ export default class IStringEscape {
      * @return {String} html解码后的字符串
      */
     static unEscapeHtml(source: string): string {
-        return '';
+        let s = "";
+        if (source.length == 0)
+            return "";
+
+        s = source.replace(/&amp;/g, "&");
+        s = s.replace(/&lt;/g, "<");
+        s = s.replace(/&gt;/g, ">");
+        s = s.replace(/&nbsp;/g, " ");
+        s = s.replace(/&#39;/g, "\'");
+        s = s.replace(/&quot;/g, "\"");
+        return s;
     }
 
     /**
--- test/IStringEscape.test.ts
+++ test/IStringEscape.test.ts
@@ -1,15 +1,21 @@
 import "mocha";
+import {expect} from "chai";
+import IStringEscape from "../src/IStringEscape";
 
 describe("escapeReg function", () => {
 
 });
 
 describe("escapeHtml function", () => {
-
+    it("basic", () => {
+        expect(IStringEscape.escapeHtml("<br>ccccc<p>aaaaa</p>")).to.be.equal("&lt;br&gt;ccccc&lt;p&gt;aaaaa&lt;/p&gt;");
+    });
 });
 
 describe("unEscapeHtml function", () => {
-
+    it("basic", () => {
+        expect(IStringEscape.unEscapeHtml("&lt;br&gt;ccccc&lt;p&gt;aaaaa&lt;/p&gt;")).to.be.equal("<br>ccccc<p>aaaaa</p>");
+    });
 });
 
 describe("escapeJavaScript function", () => {
