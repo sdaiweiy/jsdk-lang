fb9c12c57be7f75ce73701008e3a7383b29deaca
wdai <510344272@qq.com>
2018-07-14 09:56:23 +0800

    Function添加部分测试代码

--- test/IFunction.test.ts
+++ test/IFunction.test.ts
@@ -1,3 +1,5 @@
+import IFunction from "../src/IFunction";
+import {expect} from "chai";
 import "mocha";
 
 describe("currying function", () => {
@@ -5,15 +7,44 @@ describe("currying function", () => {
 });
 
 describe("delay function", () => {
+    let delayed = false;
+
+    it("basic", () => {
+        IFunction.delay(function () {
+            delayed = true;
+        }, 100);
+
+        setTimeout(function () {
+            expect(delayed).to.be.false;
+        }, 50);
+
+        setTimeout(function () {
+            expect(delayed).to.be.true;
+        }, 250);
+    });
 
 });
 
 describe("emptyFunc function", () => {
-
+    it("basic", () => {
+        expect(IFunction.emptyFunc()).to.be(void 0);
+    });
 });
 
 describe("isFunction function", () => {
+    it("basic", () => {
+        expect(IFunction.isFunction(void 0)).to.be.false;
+        expect(IFunction.isFunction([1, 2, 3])).to.be.false;
+        expect(IFunction.isFunction('moe')).to.be.false;
+        expect(IFunction.isFunction(IFunction.isFunction)).to.be.true;
+        expect(IFunction.isFunction(function () {
+        })).to.be.true;
 
+        let nodelist = typeof document != 'undefined' && document.childNodes;
+        if (nodelist) {
+            expect(IFunction.isFunction(nodelist)).to.be.false;
+        }
+    });
 });
 
 describe("bind function", () => {
